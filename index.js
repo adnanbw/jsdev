eb3cd01acbed035b5ae16d6c2b89aee93b76801a
adnanbw <rockstaradnan550@gmail.com>
2023-02-07 23:26:04 +0530

    second commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'amrit', 'build', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 65002;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);

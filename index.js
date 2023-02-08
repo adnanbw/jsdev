afd5633efa35797a18580b4eba4cf3c154662256
adnanbw <rockstaradnan550@gmail.com>
2023-02-08 21:44:54 +0530

    Added amrit dawani build

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'buildd')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'buildd', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 // app.use(express.static(path.join(__dirname, 'public_html')));
